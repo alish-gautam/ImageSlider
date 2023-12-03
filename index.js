150d95d4606779ed27d775de2733fae2733e1d29
Alish Gautam <alishgautam890@gmail.com>
2023-12-03 07:32:56 +0545

    Image slider complete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const slides = document.querySelectorAll(".slides");
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
+const dots = document.querySelectorAll(".dot");
 let index = 0;
 slides.forEach((slide, index) => {
   slide.style.left = `${index * 100}%`;
@@ -17,6 +18,8 @@ prev.addEventListener("click", () => {
     return index;
   }
   index--;
+  dotsOpacity();
+  dots[index].style.opacity = "1";
   moveSlide();
 });
 next.addEventListener("click", () => {
@@ -24,6 +27,8 @@ next.addEventListener("click", () => {
     return index;
   }
   index++;
+  dotsOpacity();
+  dots[index].style.opacity = "1";
   moveSlide();
 });
 function autoplay() {
@@ -31,8 +36,24 @@ function autoplay() {
     index = -1;
   }
   index++;
+  dotsOpacity();
+  dots[index].style.opacity = "1";
   moveSlide();
 }
 window.onload = () => {
   setInterval(autoplay, 3000);
 };
+
+function dotsOpacity() {
+  dots.forEach((dot) => {
+    dot.style.opacity = ".2";
+  });
+}
+dots.forEach((dot, i) => {
+  dot.addEventListener("click", (e) => {
+    index = i;
+    dotsOpacity();
+    e.target.style.opacity = 1;
+    moveSlide();
+  });
+});
